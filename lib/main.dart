fee09db30c78cd541cf0da175e3eae51b655af8c
Elinor Tchamo <tchamoelii@gmail.com>
2026-01-28 16:51:12 +0100

    refactor: remove unused Google Sign-In import from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mini_twitter/auth_gate.dart';
 import 'package:mini_twitter/pages/logout.dart';
 import 'firebase_options.dart';
