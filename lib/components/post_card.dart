392e815ae611c7eeb2780765f179a1b90666651e
Elinor Tchamo <tchamoelii@gmail.com>
2026-02-03 11:52:31 +0100

    feat: implement PostCard layout with user and post details

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -15,7 +15,38 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: user.photoUrl != null 
+                  ? NetworkImage(user.photoUrl!)
+                  : AssetImage('assets/images/user.png'),
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(user.pseudo, style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 4),
+                  Text('2h ago', style: TextStyle(color: Colors.grey, fontSize: 12)),
+                ]
+              )
+            ],
+          ),
+          Text(user.pseudo, style: TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(height: 8),
+          Text(post.text),
+          if (post.imageUrl != null) ...[
+            SizedBox(height: 8),
+            Image.network(post.imageUrl!),
+          ],
+          SizedBox(height: 8),
+          Text('Likes: ${post.likes}'),
+        ],
+      ),
     );
   }
 }
