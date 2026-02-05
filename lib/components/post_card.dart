c807ee52647803f857e6fa31d204eed76f62917a
Elinor Tchamo <tchamoelii@gmail.com>
2026-02-05 09:24:09 +0100

    feat: enhance PostCard layout with improved styling and interaction for like and comment actions

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -29,25 +29,71 @@ class PostCard extends StatelessWidget {
                     // ? NetworkImage(user.photoUrl!)
                     AssetImage('assets/images/user.png'),
                 ),
-                SizedBox(width: 8),
+                SizedBox(width: 10),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Pseudo", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Pseudo", style: TextStyle(color: Color(0xFF111418), fontWeight: FontWeight.bold)),
                     SizedBox(height: 4),
-                    Text('2h ago', style: TextStyle(color: Colors.grey, fontSize: 12)),
+                    Text('2h ago', style: TextStyle(color: Color(0xFF617589), fontSize: 12)),
                   ]
                 )
               ],
             ),
-            SizedBox(height: 8),
-            Text("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
             // if (post.imageUrl != null) ...[
             //   SizedBox(height: 8),
             //   Image.network(post.imageUrl!),
             // ],
-            // SizedBox(height: 8),
-            // Text('Likes: ${post.likes}'),
+            SizedBox(height: 8),
+            Text(
+              "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              style: TextStyle(color: Color(0xFF111418), fontWeight: FontWeight.w500),
+            ),
+            
+            SizedBox(height: 8),
+            Divider(thickness: 0.5,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        print("Icon clicked");
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.favorite, size: 20, color: Color(0xFF617589)),
+                          SizedBox(width: 2),
+                          Text('10'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    GestureDetector(
+                      onTap: () {
+                        print("Icon clicked");
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.comment, size: 20, color: Color(0xFF617589)),
+                          SizedBox(width: 2),
+                          Text('10'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  iconSize: 20,
+                  icon: Icon(Icons.share),
+                  color: Color(0xFF617589),
+                  onPressed: () {
+                    print("Icon clicked");
+                  },
+                ),
+              ],
+            )
           ],
         ),
       )
