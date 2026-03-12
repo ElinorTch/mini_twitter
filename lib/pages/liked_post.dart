62ed45c664bee8313bc11a7bad71e0b57a3199be
Elinor Tchamo <tchamoelii@gmail.com>
2026-03-12 21:47:05 +0100

    feat: implement LikedPostPage to display user's liked posts

---
+++ lib/pages/liked_post.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mini_twitter/components/post_card.dart';
+import 'package:mini_twitter/models/post.dart';
+import 'package:mini_twitter/models/user.dart';
+import 'package:mini_twitter/providers/current_user_provider.dart';
+
+class LikedPostPage extends StatelessWidget {
+  final List<PostModel> likedPosts;
+  final UserModel? userInfo;
+  final CurrentUserProvider currentUserProvider = CurrentUserProvider();
+
+  LikedPostPage({
+    super.key,
+    required this.likedPosts,
+    required this.userInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (likedPosts.isEmpty) {
+      return const Center(
+        child: Text(
+          "No liked posts yet",
+          style: TextStyle(color: Colors.grey),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      physics: const AlwaysScrollableScrollPhysics(),
+      itemCount: likedPosts.length,
+      itemBuilder: (context, index) {
+        final post = likedPosts[index];
+
+        return PostCard(
+          post: post,
+          user: userInfo ?? currentUserProvider.currentUser!,
+        );
+      },
+    );
+
+  }
+}
