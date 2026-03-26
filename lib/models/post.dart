8c47e979b2950e2acdc3e493d9c051307f510b65
Elinor Tchamo <tchamoelii@gmail.com>
2026-03-26 11:00:22 +0100

    Refactor: make some attribute non required

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,24 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostModel {
-  final String uid;
+  final String? uid;
   final String userId;
   final String text;
   final String? imageUrl;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final int likes;
-  final List<String> comments;
+  final DateTime createdAt = DateTime.now();
+  final DateTime updatedAt = DateTime.now();
+  final int likes = 0;
+  final List<String> comments = [];
 
   PostModel({
-    required this.uid,
     required this.userId,
     required this.text,
     this.imageUrl,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.likes,
-    required this.comments,
+    this.uid,
   });
 
   factory PostModel.fromMap(Map<String, dynamic> map, String documentId) {
@@ -28,10 +24,6 @@ class PostModel {
       userId: map['userId'],
       text: map['text'] ?? '',
       imageUrl: map['imageUrl'],
-      createdAt: (map['createdAt'] as Timestamp).toDate(),
-      updatedAt: (map['updatedAt'] as Timestamp).toDate(),
-      likes: map['likes'] ?? 0,
-      comments: List<String>.from(map['comments'] ?? []),
     );
   }
 
