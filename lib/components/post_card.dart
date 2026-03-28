02a06df2124cd8ea734ac88d06abaeb9e0cc25e9
Elinor Tchamo <tchamoelii@gmail.com>
2026-03-28 10:44:22 +0100

    Feat: add number of likes and comment to a post

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -7,11 +7,7 @@ class PostCard extends StatelessWidget {
   final UserModel user;
   final PostModel post;
 
-  const PostCard({
-    super.key,
-    required this.user,
-    required this.post,
-  });
+  const PostCard({super.key, required this.user, required this.post});
 
   @override
   Widget build(BuildContext context) {
@@ -27,38 +23,55 @@ class PostCard extends StatelessWidget {
                 CircleAvatar(
                   backgroundColor: Color(0xFFf6f7f8),
                   backgroundImage: user.photoUrl != null
-                    ? NetworkImage(user.photoUrl!)
-                    : null,
+                      ? NetworkImage(user.photoUrl!)
+                      : null,
                   child: user.photoUrl == null
-                    ? Image.asset('assets/images/person.png')
-                    : null,
+                      ? Image.asset('assets/images/person.png')
+                      : null,
                 ),
                 SizedBox(width: 10),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(user.pseudo, style: TextStyle(color: Color(0xFF111418), fontWeight: FontWeight.bold)),
+                    Text(
+                      user.pseudo,
+                      style: TextStyle(
+                        color: Color(0xFF111418),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                     SizedBox(height: 4),
-                    Text(timeAgo(post.createdAt), style: TextStyle(color: Color(0xFF617589), fontSize: 12)),
-                  ]
-                )
+                    Text(
+                      timeAgo(post.createdAt),
+                      style: TextStyle(color: Color(0xFF617589), fontSize: 12),
+                    ),
+                  ],
+                ),
               ],
             ),
             if (post.imageUrl != null) ...[
               SizedBox(height: 8),
               ClipRRect(
                 borderRadius: BorderRadius.circular(8),
-                child: Image.network(post.imageUrl!, width: double.infinity, fit: BoxFit.cover, height: 200)
-              )
+                child: Image.network(
+                  post.imageUrl!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  height: 200,
+                ),
+              ),
             ],
             SizedBox(height: 8),
             Text(
               post.text,
-              style: TextStyle(color: Color(0xFF111418), fontWeight: FontWeight.w500),
+              style: TextStyle(
+                color: Color(0xFF111418),
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            
+
             SizedBox(height: 8),
-            Divider(thickness: 0.5,),
+            Divider(thickness: 0.5),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -73,11 +86,15 @@ class PostCard extends StatelessWidget {
                         padding: EdgeInsets.all(8),
                         child: Row(
                           children: [
-                            Icon(Icons.favorite, size: 20, color: Color(0xFF617589)),
+                            Icon(
+                              Icons.favorite,
+                              size: 20,
+                              color: Color(0xFF617589),
+                            ),
                             SizedBox(width: 2),
-                            Text('10'),
+                            Text('${post.likes}'),
                           ],
-                        )
+                        ),
                       ),
                     ),
                     InkWell(
@@ -89,13 +106,17 @@ class PostCard extends StatelessWidget {
                         padding: EdgeInsets.all(8),
                         child: Row(
                           children: [
-                            Icon(Icons.comment, size: 20, color: Color(0xFF617589)),
+                            Icon(
+                              Icons.comment,
+                              size: 20,
+                              color: Color(0xFF617589),
+                            ),
                             SizedBox(width: 2),
-                            Text('10'),
+                            Text('${post.comments.length}'),
                           ],
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
                 IconButton(
@@ -107,10 +128,10 @@ class PostCard extends StatelessWidget {
                   },
                 ),
               ],
-            )
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
